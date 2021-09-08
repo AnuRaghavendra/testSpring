9706af78fa45e69c74882e5f51c481278d18cc8b
AnuRaghavendra <anuraghavendra95@gmail.com>
2021-09-08 17:45:40 +0530

    start demo

--- src/main/java/startspring/StartSpringDemo.java
+++ src/main/java/startspring/StartSpringDemo.java
@@ -9,7 +9,7 @@ public class StartSpringDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(StartSpringDemo.class, args);
-		System.out.println("Spring Demo!");
+		System.out.println("Spring Start Demo!");
 	}
 
 }
