05098cd17963db6955bff806238a3cfa8f76f8bb
AnuRaghavendra <anuraghavendra95@gmail.com>
2021-09-08 16:51:10 +0530

    start spring demo

---
+++ src/main/java/startspring/StartSpringDemo.java
@@ -0,0 +1,15 @@
+package startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(StartSpringDemo.class, args);
+		System.out.println("Spring Demo");
+	}
+
+}
